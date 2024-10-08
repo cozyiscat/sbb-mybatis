15d53421ae9e697ad3daad33a5f2ee2e6e12c2bc
cozyiscat <yuiop7664@gmail.com>
2024-10-08 11:45:45 +0900

    db 연동 mapper 만들기

---
+++ src/main/java/com/mysite/sbbmybatis/User.java
@@ -0,0 +1,43 @@
+package com.mysite.sbbmybatis;
+
+import java.time.LocalDateTime;
+
+public class User {
+	private Integer id; //7
+	private String username; //hong
+	private String pwd; //1234
+	private LocalDateTime createdAt; //created_at 카멜표기법
+	private LocalDateTime updatedAt;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+}

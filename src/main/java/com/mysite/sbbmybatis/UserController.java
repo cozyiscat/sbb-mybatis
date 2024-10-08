a8885674ca3f6198e2fd4fd7095276e90e2f4d74
cozyiscat <yuiop7664@gmail.com>
2024-10-08 14:11:16 +0900

    처음으로 초기화해서 다시 만들기

--- src/main/java/com/mysite/sbbmybatis/UserController.java
+++ src/main/java/com/mysite/sbbmybatis/UserController.java
@@ -1,23 +1,12 @@
 package com.mysite.sbbmybatis;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UserController {
-	@Autowired
-	private UserService userService;
-	
-	@GetMapping ("/user/login")
+	@GetMapping("/user/login") 
 	public String login() {
 		return "/user/login";
 	}
-	
-    @GetMapping("/user/{username}")
-    public User getMessage(@PathVariable("username") String username) {
-        return userService.getUser(username);
-    }
-
 }
